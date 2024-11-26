94c4e0bae0b7cec2574c74d67cc9cb470e85c712
Gregoire <wi22b060@technikum-wien.at>
2024-11-26 22:14:14 +0100

    test

--- __main__.py
+++ __main__.py
@@ -14,9 +14,15 @@ if os.path.exists(zip_file_path):
 # ZIP-Datei der Anwendung erstellen
 if os.path.exists('clco-demo'):
     shutil.make_archive('webapp', 'zip', 'clco-demo')
+    print(f"ZIP-Datei '{zip_file_path}' wurde erfolgreich erstellt.")
 else:
     raise FileNotFoundError("Das Verzeichnis 'clco-demo' wurde nicht gefunden.")
 
+# Überprüfen, ob die ZIP-Datei erstellt wurde und nicht leer ist
+if os.path.exists(zip_file_path) and os.path.getsize(zip_file_path) > 0:
+    print(f"ZIP-Datei '{zip_file_path}' wurde erfolgreich erstellt und ist nicht leer.")
+else:
+    raise FileNotFoundError("Die ZIP-Datei wurde nicht erstellt oder ist leer.")
 
 # Erstellen einer Ressourcengruppe
 resource_group = resources.ResourceGroup("uebung4-resourcegroup", location="canadacentral")
@@ -37,12 +43,12 @@ blob_container = storage.BlobContainer("blobcontainer",
     public_access="Blob"
 )
 
-# Hochladen einer Datei in den Blob-Container  
+# Hochladen einer Datei in den Blob-Container
 app_blob = storage.Blob("webappzip",
     resource_group_name=resource_group.name,
     account_name=storage_account.name,
     container_name=blob_container.name,
-    source=FileAsset("./webapp.zip")
+    source=FileAsset(zip_file_path)
 )
 
 # Generieren der Blob-URL
@@ -55,12 +61,19 @@ app_service_plan = web.AppServicePlan("serviceplan",
     kind="Linux",
     reserved=True,
     sku=web.SkuDescriptionArgs(
-       tier="Basic",
-        name="B1",
+        tier="Standard",
+        name="S1",
     )
 )
 
-
+# Application Insights erstellen
+app_insights = insights.Component("appinsights",
+    resource_group_name=resource_group.name,
+    location=resource_group.location,
+    application_type="web",
+    kind="web",
+    ingestion_mode="ApplicationInsights"
+)
 
 # Erstellen einer Web-App
 web_app = web.WebApp("webapp",
@@ -70,6 +83,7 @@ web_app = web.WebApp("webapp",
     site_config=web.SiteConfigArgs(
         app_settings=[
             web.NameValuePairArgs(name="WEBSITE_RUN_FROM_PACKAGE", value=blob_url),
+            web.NameValuePairArgs(name="APPINSIGHTS_INSTRUMENTATIONKEY", value=app_insights.instrumentation_key),
         ],
         linux_fx_version="PYTHON|3.11",
     )
